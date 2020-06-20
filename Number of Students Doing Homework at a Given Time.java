45c83db3106c07cac1abe3e680393cccac13cae1
urjitdesai <44604685+urjitdesai@users.noreply.github.com>
2020-06-20 13:35:58 +0530

    Create Number of Students Doing Homework at a Given Time.java

---
+++ Number of Students Doing Homework at a Given Time.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int c=0;
+        for(int i=0;i<endTime.length;i++){
+            if(queryTime >= startTime[i] && queryTime<= endTime[i])
+                c++;
+        }
+        return c;
+    }
+}
